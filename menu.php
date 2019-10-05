f82570b4fc9ed8f39376f6b18b638af3c204a96f
Ben McFadden <viscid07@gmail.com>
2019-10-04 20:13:26 -0400

    added draft image slideshow to graphic page

--- menu.php
+++ menu.php
@@ -3,7 +3,7 @@
 		<li><a href="graphic.php">graphic work</a></li>
 		<li><a href="web.php">web work</a></li>
 		<li><a href="project.php">project management</a></li>				
-		<li><a href="contact.php">Contact</a></li>
+		<!--<li><a href="contact.php">Contact</a></li>-->
 		<li><a href="career.php">Career</a></li>
 		<li><a href="Documents/resume.pdf">My Resume</a></li>
 	</ul>
