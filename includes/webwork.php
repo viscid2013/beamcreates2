83dbdc99748174079f47e90cf36ebf4d99fd7969
Ben McFadden <viscid07@gmail.com>
2019-10-05 13:17:22 -0400

    initial content draft on web work page

--- includes/webwork.php
+++ includes/webwork.php
@@ -15,7 +15,11 @@
 		I create web interfaces using HTML/Responsive CSS with MySQL/PHP-PDO and Javascript/jQuery. I did much of my early coding work as a Digital Librarian with Wake County Public Libraries, before taking on a management and administration role overseeing the department whose work includes handling WCPL's web presence. I am in the process of working on personal projects to deepen and enhance my knowledge and skills in these areas.
 		</p>
 		<p>Web Work with the Public Libraries</p>
-		<p>[images and links to show examples]</p>
+		<p><a href="http://digwebcode.wakegov.com/src/SAP/suggest_form_hx.php?showSecretForm=yes">Suggest a Purchase System</a></p>
+		<p>Suggest a Purchase was a system built to manage suggestions from patrons as to books and other library materials for purchase.</p>
+		<p>The front end includes an entry form for the suggestions that uses the Google Books API to pull author and title information using the ISBN as well as an ODBC connection to the Intergrated Library System database to check whether or not the item is already owned by the lirbary system.</p>
+		<p>On the staff side, the requests are stored in a database and displayed in table allowing for viewing details and editing certain fields in order to process the requests. The key function is to indicate the status of the item, i.e. whether or not is has been selected for purchase. Depending on the status, different email messages are automatically sent to the requesting patrons.</p>
+		<p>NOTE: We recently transitioned the Suggest a Purchase system to a module built into our vendor-provided online catalog. This does mean some loss of customization, but it creates a more unified experience and less maintenance work for me!</p>
 		<p>Personal Projects</p>
 		<p>[Logo/link and summary for ViscidCreates and logo/summary for upcoming Verses Project]</p>
 	</div>
--- menu.php
+++ menu.php
@@ -1,9 +1,9 @@
 	<ul>
-		<li><a href="index.php">Home</a></li>
-		<li><a href="graphic.php">graphic work</a></li>
-		<li><a href="web.php">web work</a></li>
-		<li><a href="project.php">project management</a></li>				
+		<li><a href="index.php">&#124;Home</a></li>
+		<li><a href="graphic.php">&#124;graphic work</a></li>
+		<li><a href="web.php">&#124;web work</a></li>
+		<li><a href="project.php">&#124;project management</a></li>				
 		<!--<li><a href="contact.php">Contact</a></li>-->
-		<li><a href="career.php">Career</a></li>
-		<li><a href="Documents/resume.pdf">My Resume</a></li>
+		<li><a href="career.php">&#124;Career</a></li>
+		<li><a href="Documents/resume.pdf">&#124;My Resume</a></li>
 	</ul>
