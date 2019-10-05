5a96fddbea93aa21b53a352010c9c40f82fc1d66
Ben McFadden <viscid07@gmail.com>
2019-10-05 16:27:53 -0400

    sub heads font weight 400

--- includes/webwork.php
+++ includes/webwork.php
@@ -14,13 +14,13 @@
 		<p>
 		I create web interfaces using HTML/Responsive CSS with MySQL/PHP-PDO and Javascript/jQuery. I did much of my early coding work as a Digital Librarian with Wake County Public Libraries, before taking on a management and administration role overseeing the department whose work includes handling WCPL's web presence. I am in the process of working on personal projects to deepen and enhance my knowledge and skills in these areas.
 		</p>
-		<p class="w3-padding-small w3-border-bottom" style="font-weight: 600;">Web Work with the Public Libraries</p>
+		<p class="w3-padding-small w3-border-bottom" style="font-weight: 400;">Web Work with the Public Libraries</p>
 		<p><a href="http://digwebcode.wakegov.com/src/SAP/suggest_form_hx.php?showSecretForm=yes">Suggest a Purchase System</a></p>
 		<p>Suggest a Purchase was a system built to manage purchase recommendations from library patrons.</p>
 		<p>The front end includes an entry form for the suggestions that uses the Google Books API to pull author and title information using the ISBN as well as an ODBC connection to the Intergrated Library System database to check whether or not the item is already owned by the libraries.</p>
 		<p>On the staff side, the stored requests are displayed in a table for viewing and editing. When the status of an item is changed to indicate whether or not it will be purchased, a relevant email message is automatically sent to the requesting patrons.</p>
 		<p>NOTE: We recently transitioned the Suggest a Purchase system to a module built into our vendor-provided online catalog. This does mean some loss of customization, but it creates a more unified experience and less maintenance work for me!</p>
-		<p class="w3-padding-small w3-border-bottom" style="font-weight: 600;">Personal Projects</p>
+		<p class="w3-padding-small w3-border-bottom" style="font-weight: 400;">Personal Projects</p>
 		<p>[Logo/link and summary for ViscidCreates and logo/summary for upcoming Verses Project]</p>
 	</div>
 	
