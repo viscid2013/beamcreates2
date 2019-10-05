9eb04cdf43daed7e632564b9c6bb57158ec62b22
Ben McFadden <viscid07@gmail.com>
2019-10-05 16:26:05 -0400

    h2 for header and font-weight 600 for sub headings

--- includes/webwork.php
+++ includes/webwork.php
@@ -7,20 +7,20 @@
 
 <body>
 	
-	<h3>Web Work</h3>
+	<h2>Web Work</h2>
 
 <img src="../branding_icons/mecoding.png" alt="bearded coding" style="max-width: 50%;" />
 	<div>
 		<p>
 		I create web interfaces using HTML/Responsive CSS with MySQL/PHP-PDO and Javascript/jQuery. I did much of my early coding work as a Digital Librarian with Wake County Public Libraries, before taking on a management and administration role overseeing the department whose work includes handling WCPL's web presence. I am in the process of working on personal projects to deepen and enhance my knowledge and skills in these areas.
 		</p>
-		<p class="w3-padding-small w3-border-bottom" style="font-weight: bold;">Web Work with the Public Libraries</p>
+		<p class="w3-padding-small w3-border-bottom" style="font-weight: 600;">Web Work with the Public Libraries</p>
 		<p><a href="http://digwebcode.wakegov.com/src/SAP/suggest_form_hx.php?showSecretForm=yes">Suggest a Purchase System</a></p>
-		<p>Suggest a Purchase was a system built to manage suggestions from patrons as to books and other library materials for purchase.</p>
-		<p>The front end includes an entry form for the suggestions that uses the Google Books API to pull author and title information using the ISBN as well as an ODBC connection to the Intergrated Library System database to check whether or not the item is already owned by the library system.</p>
-		<p>On the staff side, the requests are stored in a database and displayed in table allowing for viewing details and editing certain fields in order to process the requests. The key function is to indicate the status of the item, i.e. whether or not is has been selected for purchase. Depending on the status, different email messages are automatically sent to the requesting patrons.</p>
+		<p>Suggest a Purchase was a system built to manage purchase recommendations from library patrons.</p>
+		<p>The front end includes an entry form for the suggestions that uses the Google Books API to pull author and title information using the ISBN as well as an ODBC connection to the Intergrated Library System database to check whether or not the item is already owned by the libraries.</p>
+		<p>On the staff side, the stored requests are displayed in a table for viewing and editing. When the status of an item is changed to indicate whether or not it will be purchased, a relevant email message is automatically sent to the requesting patrons.</p>
 		<p>NOTE: We recently transitioned the Suggest a Purchase system to a module built into our vendor-provided online catalog. This does mean some loss of customization, but it creates a more unified experience and less maintenance work for me!</p>
-		<p class="w3-padding-small w3-border-bottom" style="font-weight: bold;">Personal Projects</p>
+		<p class="w3-padding-small w3-border-bottom" style="font-weight: 600;">Personal Projects</p>
 		<p>[Logo/link and summary for ViscidCreates and logo/summary for upcoming Verses Project]</p>
 	</div>
 	
