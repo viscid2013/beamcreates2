0223adc4e8402aaf57e46d212aeabd76ca1ad7fc
Ben McFadden <viscid07@gmail.com>
2019-10-11 17:11:17 -0400

    trying to scroll mainConten instead of body?

--- includes/beamscript.php
+++ includes/beamscript.php
@@ -18,7 +18,7 @@ $(document).ready(function(){
 
       // Using jQuery's animate() method to add smooth page scroll
       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
+      $('#mainContent').animate({
         scrollTop: $(hash).offset().top
       }, 800, function(){
 
