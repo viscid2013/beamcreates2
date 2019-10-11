f4da17755bed16ed2f15deee7c50edb4a9a82d37
Ben McFadden <viscid07@gmail.com>
2019-10-11 09:11:42 -0400

    added border bottom to sections on project and web stuff - spell checked content

--- includes/webwork.php
+++ includes/webwork.php
@@ -17,15 +17,15 @@
 		<h3 class="w3-padding-small w3-border-bottom w3-border-top w3-text-theme" style="font-weight: bold;">Personal Projects</h3>
 			<p style="font-style: italic; font-weight: bold;">Current Experimental Projects:</p>
 				<p><a href=""><img src="../branding_icons/vc_logotype.svg" alt="viscid creates logo" style="width: 75%;" /></a></p>
-				<p>Summary:</p>
+				<p class="w3-border-bottom w3-padding-small">Summary:</p>
 					<p>Viscid Creates is an experimental online graphics portfolio and store created as a testing and learning ground for refining and practicing my skills.</p>
-				<p>Code:</p>
+				<p class="w3-border-bottom w3-padding-small">Code:</p>
 					<ul>
 					<li>HTML and W3-CSS for responsive design</li>
 					<li>Plain Javascript (mostly, see notes) for DOM manipulation and enhanced funtionality</li>
 					<li>PHP with PDO/MySQL</li>
 					</ul>
-				<p>Core Features:</p>
+				<p class="w3-border-bottom w3-padding-small">Core Features:</p>
 					<ul>
 					<li>Responsive display of squared, tiled images that can be searched by keyword and filtered by available print size.</li>
 					<li>Images can be expanded into a modal slideshow on larger screens.</li>
@@ -35,7 +35,7 @@
 					<li>Shopping cart: add, view, remove.</li>
 					<li>Contact forms for custom work and general queries.</li>
 					</ul>
-				<p>Notes:</p>
+				<p class="w3-border-bottom w3-padding-small">Notes:</p>
 					<ul>
 					<li>Viscid Creates is about 85% functional, and I might revisit in future to flesh it out beyond the experimental stage.</li>
 					<li>I set out to use pure Javascript as much as possible for the DOM manipulation, but did end up incorporating and modifying some instances of pre-built jQuery and Javascript for editing and displaying user avatars and for a custom accordion. Credit is given in the code comments.</li>
@@ -51,16 +51,16 @@
 		<h3 class="w3-padding-small w3-border-bottom w3-border-top w3-text-theme" style="font-weight: bold;">Web Work with the Public Libraries</h3>
 			<p style="font-style: italic; font-weight: bold;">Part 1: Discontinued but still viewable/partly functional interfaces:</p>
 				<p class="w3-padding-small w3-text-theme"><a href="http://digwebcode.wakegov.com/src/SAP/suggest_form_hx.php?showSecretForm=yes"><img src="../web_images/wcpl_sap.png" alt="wcpl suggest a purchase logo" style="max-width: 100%;" /></a></p>
-						<p>Summary:</p>
+						<p class="w3-border-bottom w3-padding-small">Summary:</p>
 							<p>I built the Suggest a Purchase System to manage purchase recommendations from library patrons.</p>
-						<p>Code:</p>
+						<p class="w3-border-bottom w3-padding-small">Code:</p>
 							<ul>
 							<li>HTML, CSS, and Javascript</li>
 							<li>PHP/MySQL </li>
 							<li>ODBC Connection to the Horizon Integrated Library System</li>
 							<li>Google Books API integration</li>
 							</ul>
-						<p>Core Features:</p>
+						<p class="w3-border-bottom w3-padding-small">Core Features:</p>
 							<ul>
 							<li>Suggest a Purchase form to allow materials purchase recommendations from patrons.</li>
 							<li>ODBC connection to the ILS allows check via ISBN if library already owns title.</li>
@@ -68,44 +68,44 @@
 							<li>Staff interface to view and update requests.</li>
 							<li>Automated email messages specific to request status sent on status change to requesting users.</li>
 							</ul>
-						<p>Notes:</p>
+						<p class="w3-border-bottom w3-padding-small">Notes:</p>
 							<p>We recently transitioned the Suggest a Purchase system to a module built into our vendor-provided online catalog. This does mean some loss of customization, but it creates a more unified experience and less maintenance work for me!</p>
 				<p class="w3-padding-small w3-text-theme"><a href="http://digwebcode.wakegov.com/pr/prform.php"><img src="../web_images/wcpl_pr.png" alt="wcpl pr request logo" style="max-width: 100%;" /></a></p>
-					<p>Summary:</p>
+					<p class="w3-border-bottom w3-padding-small">Summary:</p>
 							<p>I created the PR Request System to manage marketing requests for library events and programs from library managers.</p>
-						<p>Code:</p>
+						<p class="w3-border-bottom w3-padding-small">Code:</p>
 							<ul>
 							<li>HTML, CSS, and Javascript/jQuery</li>
 							<li>PHP/MySQL </li>
 							</ul>
-						<p>Core Features:</p>
+						<p class="w3-border-bottom w3-padding-small">Core Features:</p>
 							<ul>
 							<li>PR Request Form, which solicits the relevant information about the event, including times and locations, contact info, summaries, program type, etc.</li>
 							<li>PR Calendar to display/access results on specified dates, custom-built with PHP and Javascript. (I don't plan to attempt that again anytime soon! It 95% works, but there were always some quirks.)</li>
 							<li>I created a searchable table display of stored requests.</li>
 							<li>Event detail pages for viewing editing particulars.</li>
 							</ul>
-						<p>Notes:</p>
+						<p class="w3-border-bottom w3-padding-small">Notes:</p>
 							<p>As with the Suggest a Purchase System, my switch to management and administrative duties left me unable to keep the PR Request system properly updated and maintained, so we switched to using a system created in Microsoft Sharepoint by our County IS department. I migrated the PR Request System to a new server with a newer version of PHP, so while the historical instance still works, mostly, it has some issues. &#58;&#41;</p>
 		
 			<p class="" style="font-style: italic; font-weight: bold;">Part 2: Discontinued but no longer existing or no longer functioning:</p>
 				<p class="w3-padding-small"><img src="../web_images/wcpl_tp.png" alt="wcpl teen poetry contest logo" style="max-width: 100%;" /></p>
-					<p>Summary:</p>
+					<p class="w3-border-bottom w3-padding-small">Summary:</p>
 							<p>I created the Teen Poetry Contest site to manage poetry submissions for an annual library teen poetry contest.</p>
-						<p>Code:</p>
+						<p class="w3-border-bottom w3-padding-small">Code:</p>
 							<ul>
 							<li>HTML, CSS, and Javascript/jQuery</li>
 							<li>PHP/MySQL </li>
 							<li>Open source WYSIWYG editor plugin.</li>
 							</ul>
-						<p>Core Features:</p>
+						<p class="w3-border-bottom w3-padding-small">Core Features:</p>
 							<ul>
 							<li>A poem submission form that integrated a WYSIWYG editor.</li>
 							<li>A back end system for judges to organize, view, and rate submissions.</li>
 							<li>Individual logins with specific permissions for contest admins, staff judges, and volunteer judges. Judges could only view the poems for the age category they were judging, and could only see their own ratings.</li>
 							<li>Once the winners were selected, those poems could be marked for display on the library website.</li>
 							</ul>
-						<p>Notes:</p>
+						<p class="w3-border-bottom w3-padding-small">Notes:</p>
 							<ul>
 							<li>WCPL's Teen Poetry Contest was originally tracked on a gigantic spreadsheet. I think maybe there was a big binder involved?</li>
 							<li>This one did a lot of cool stuff, even if I would be embarrassed for anyone to see the actual code. &#58;&#124;</li>
@@ -113,21 +113,21 @@
 							</ul>
 
 				<p class="w3-padding-small"><img src="../web_images/wcpl_get.png" alt="wcpl getterizer logo" style="max-width: 100%;" /></p>
-					<p>Summary:</p>
+					<p class="w3-border-bottom w3-padding-small">Summary:</p>
 							<p>I created the Getterizer to offer the librarians a way to easily create reading lists for display online.</p>
-						<p>Code:</p>
+						<p class="w3-border-bottom w3-padding-small">Code:</p>
 							<ul>
 							<li>HTML, CSS, and Javascript/jQuery</li>
 							<li>PHP/MySQL </li>
 							<li>Open source API to fetch book info via ISBN</li>
 							</ul>
-						<p>Core Features:</p>
+						<p class="w3-border-bottom w3-padding-small">Core Features:</p>
 							<ul>
 							<li>A form that accepted entry of one or more ISBNs to generate and store a list of titles with cover, author, and summary info using an open source API.</li>
 							<li>The list info was stored in a database so that the lists could be displayed by category (chosen in the submission process) on the library website.</li>
 							<li>The lists started as fairly simple, web-appropriate affairs, but evolved to include a basic level of customizable styling for printing.</li>
 							</ul>
-						<p>Notes:</p>
+						<p class="w3-border-bottom w3-padding-small">Notes:</p>
 							<ul>
 							<li>This was one of the first interfaces I built in my work with Wake County Public Libraries.</li>
 							<li>Prior to this, the lists were created in Publisher and converted to PDF's for manual upload to the website. This was labor-intensive and not user-friendly.</li>
