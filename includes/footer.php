ca260626613d3f2501df8b0f94b199cdb6d66738
Ben McFadden <viscid07@gmail.com>
2019-10-11 16:16:06 -0400

    testing scroll to top smoothly thingy

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,30 @@
 <div id="footer" class="w3-theme-l1 w3-padding w3-center">
+	<div class="w3-padding-small w3-text-theme" id="scrollTop">TOP!</div>
 <?php include("menu.php"); ?>	
 </div>
+<script>
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("#scrollTop").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
+</script>
--- includes/header.php
+++ includes/header.php
@@ -1,7 +1,8 @@
 <head>
 <meta charset="utf-8">
 	
-<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js"></script>
+<!--<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js"></script>-->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	
 <meta name="viewport" content="width=device-width, initial-scale=1">
 	
