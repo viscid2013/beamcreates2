92c4b0d8b49706d385815b033b33d7e9610391dd
Ben McFadden <viscid07@gmail.com>
2019-10-11 07:52:37 -0400

    border bottom to section headings

--- includes/projectmgt.php
+++ includes/projectmgt.php
@@ -15,7 +15,7 @@
 		In my work with public libraries, I plan, partner in, and oversee multiple projects relating to IT and web presence, as well as marketing and awareness of library programs and services.
 		</p>
 		<h3 class="w3-padding-small w3-border-bottom w3-border-top w3-text-theme" style="font-weight: bold;">Wake County Public Libraries Projects</h3>
-			<p style="font-style: italic; font-weight: bold;">Transfer of Library Servers to County Datacenter</p>
+			<p class="w3-border-bottom w3-padding-small" style="font-style: italic; font-weight: bold;">Transfer of Library Servers to County Datacenter</p>
 				<p class="w3-border-bottom w3-padding-small">My role: </p>
 				<p>Project lead for Library IT</p>
 				<p class="w3-border-bottom w3-padding-small">Other roles:</p>
@@ -28,29 +28,29 @@
 					<p>The small datacenter at the library administration building that housed the library servers was aging, lacking robust data and power backup. Additionally, with staffing levels in Library IT, it was difficult to properly integrate the servers with the workflow and update processes of County IS, this fully benefit from the established broader server ecosystem. We reached out to County IS to explore transferring the library servers to the County's main datacenter, where they could be integrated into the main workflow of updates and patching, and benefit from the more robust power and data backups.</p>
 					<p>What started out as a daunting task ended up as a great success, and a stellar example of teamwork! Everyone involved brought their expertise and creativity to bear, and we systematically evaluated and addressed each challenge and step of the process. What hiccups arose were handled calmly, and the new environment has been running smoothly for a few years. No longer does a power outage require a scramble to safely bring down the library servers!</p>
 	
-			<p style="font-style: italic; font-weight: bold;">Coordination with Marketing Firm</p>
-				<p>My role: </p>
-				<p>Co-Project Lead for Libraries along the Library Deputy Director</p>
-				<p>Other roles:</p>
+			<p class="w3-border-bottom w3-padding-small" style="font-style: italic; font-weight: bold;">Coordination with Marketing Firm</p>
+				<p class="w3-border-bottom w3-padding-small">My role: </p>
+				<p>Co-Project Lead for Libraries alongside the Library Deputy Director</p>
+				<p class="w3-border-bottom w3-padding-small">Other roles:</p>
 				<ul>
 					<li>Digital Resource Manager for Awareness with Libraries: oversaw integration of marketing firm materials, branding, and themes with libraries internal marketing materials and campaigns.</li>
 					<li>Libraries Liaison with County Communications Office: represented County interests in external marketing and branding</li>
 					<li>Project manager from the marketing firm</li>
 					<li>Media and Creative consultants from the marketing firm</li>
 				</ul>
-				<p>Summary: </p>
+				<p class="w3-border-bottom w3-padding-small">Summary: </p>
 					<p>While the libraries have a strong marketing and awareness team for internal promotion and branding of library programs and services, we have traditionally lacked the time and resources for a large, traditional, external campaign. To this end, we experimented with a short term partnership with an external marketing firm to explore new avenues of promotion and raising awareness of our libraries. There were interesting challenges with coordinating efforts, integrating our already distinctive design efforts with the styles presented by the firm, finding ways to adjust techniques and methods for a government entity, and libraries in particular. The County had certain standards and requirements to maintain, for example. Overall, it was a a great learning experience, and even after the contract ended, we ended up with a mix of validation of previous efforts and new tools and methods to apply going forward. </p>
 
-			<p style="font-style: italic; font-weight: bold;">Restructure of Library Technology Department</p>
-				<p>My role: </p>
+			<p class="w3-border-bottom w3-padding-small" style="font-style: italic; font-weight: bold;">Restructure of Library Technology Department</p>
+				<p class="w3-border-bottom w3-padding-small">My role: </p>
 				<p>Senior Library Manager for Technology (Interim at the time)</p>
-				<p>Other roles:</p>
+				<p class="w3-border-bottom w3-padding-small">Other roles:</p>
 				<ul>
 					<li>Library Data and Policy Manager: temporarily assigned to oversee and evaluate Library IT to advise on structure and efficiencies</li>
 					<li>Library IT Specialist/Supervisor: advise on existing Library IT practices and serve as team leader for field projects</li>
 					<li>Library IT Technicians: primary implementation of helpdesk and support duties; advist on existing practices</li>
 				</ul>
-				<p>Summary: While the library system and it's tech support needs had grown steadily, the team assigned to support them had not grown in keeping with those needs. The data and policy manager was temporarily assigned to work with the Library IT Specialist who served as supervisor and team leader to identify ways that the team could operate in a more structured and efficient manner and communicate more effectively, as well as identify potential staffing and resource needs. It was during this time that I became the Interim Senior Manger for Digital Infrastructure. Initially, I partnered with the Data and Policy Manager and Library IT Specialist to track and improve support and support results data, and to advise on general structural and resource needs. Ultimately, the Data and Policy Manager returned to regular duties so that I could pick up where he left off and carry on to next steps. After a few months of further evaluation, I worked with library and County administration to develop a revised structure for the Library Technology/Digital Library Departments, unifying the unit under the name Digital Infrastructure (to capute those aspects that were not related to traditional IT roles), and ulimately establishing a role of Library IT Project Mangager, hiring an additional Library IT Specialist from within the ranks, and adding two additional Technician positions. Between the restructure of staffing and enhanced communication and efficiencies, IT support and satsifaction with such has increased, and the team has been smoothly handling multiple simultaneous special projects in addition to their daily support duties.</p>
+				<p class="w3-border-bottom w3-padding-small">Summary: While the library system and it's tech support needs had grown steadily, the team assigned to support them had not grown in keeping with those needs. The data and policy manager was temporarily assigned to work with the Library IT Specialist who served as supervisor and team leader to identify ways that the team could operate in a more structured and efficient manner and communicate more effectively, as well as identify potential staffing and resource needs. It was during this time that I became the Interim Senior Manger for Digital Infrastructure. Initially, I partnered with the Data and Policy Manager and Library IT Specialist to track and improve support and support results data, and to advise on general structural and resource needs. Ultimately, the Data and Policy Manager returned to regular duties so that I could pick up where he left off and carry on to next steps. After a few months of further evaluation, I worked with library and County administration to develop a revised structure for the Library Technology/Digital Library Departments, unifying the unit under the name Digital Infrastructure (to capute those aspects that were not related to traditional IT roles), and ulimately establishing a role of Library IT Project Mangager, hiring an additional Library IT Specialist from within the ranks, and adding two additional Technician positions. Between the restructure of staffing and enhanced communication and efficiencies, IT support and satsifaction with such has increased, and the team has been smoothly handling multiple simultaneous special projects in addition to their daily support duties.</p>
 	
 
 	</div>
