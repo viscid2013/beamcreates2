bd48abca0fcb390680d1059d43baec546a727584
Ben McFadden <viscid07@gmail.com>
2019-10-12 06:52:07 -0400

    new code for scroll top test

--- test/scroll_test.php
+++ test/scroll_test.php
@@ -4,11 +4,20 @@
 <meta charset="utf-8">
 
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+<style>
+    .scroll-top{
+        position: fixed;
+        bottom: 10px;
+        right: 10px;
+        z-index: 99;
+    }
+</style>
 	
 <title>Scroll Test</title>
 </head>
 
-<body>
+<body style="width: 50%; margin-top: 25px;">
 	
 <p>
 	
@@ -229,35 +238,237 @@ Integrated Library System software).
 	
 </p>
 	
-<div class="w3-padding-small w3-text-theme scrollTop">TOP!</div>
+<p>
+	
+HOME
+I am a dad, an artist, a writer, a librarian, 
+a web coder, a casual gamer, and just the
+right kind of neurotic that I can function 
+as if naturally disciplined and organized.
+	</p>
+<p>
+I worked in social work for several years after
+college before discovering my career in public
+libraries. After some years in a public facing
+role working with all ages in various roles,
+I began work in digital services, and soon 
+moved into management and administration.
+
+	</p>
+<p>
+WORK &amp; CAREER
+I originally studied religion and psychology,
+and spent several years doing inpatient and
+outpatient social work. But since 2003, I have
+been working in Public Libraries, first in a
+public facing role, planning and running 
+programs for children, teens, and adults. 
+	</p>
+<p>
+In 2010, I was recruited for my library system's
+Digital Library, providing support to staff and
+patrons related to our digital services, as well
+as creating custom web interfaces for internal
+and public use. 
+	</p>
+<p>
+In 2014, I began managing the Digital Library
+before moving into a interim role as Senior
+Library Manager for Digital Infrastructure, 
+overseeing Library IT, Marketing &amp; Awareness,
+and Library Information Services (responsible 
+for the library system's web presence and the
+Integrated Library System software). 
+	
+</p>
+	
+<p>
+	
+HOME
+I am a dad, an artist, a writer, a librarian, 
+a web coder, a casual gamer, and just the
+right kind of neurotic that I can function 
+as if naturally disciplined and organized.
+	</p>
+<p>
+I worked in social work for several years after
+college before discovering my career in public
+libraries. After some years in a public facing
+role working with all ages in various roles,
+I began work in digital services, and soon 
+moved into management and administration.
+
+	</p>
+<p>
+WORK &amp; CAREER
+I originally studied religion and psychology,
+and spent several years doing inpatient and
+outpatient social work. But since 2003, I have
+been working in Public Libraries, first in a
+public facing role, planning and running 
+programs for children, teens, and adults. 
+	</p>
+<p>
+In 2010, I was recruited for my library system's
+Digital Library, providing support to staff and
+patrons related to our digital services, as well
+as creating custom web interfaces for internal
+and public use. 
+	</p>
+<p>
+In 2014, I began managing the Digital Library
+before moving into a interim role as Senior
+Library Manager for Digital Infrastructure, 
+overseeing Library IT, Marketing &amp; Awareness,
+and Library Information Services (responsible 
+for the library system's web presence and the
+Integrated Library System software). 
+	
+</p>
+	
+<p>
+	
+HOME
+I am a dad, an artist, a writer, a librarian, 
+a web coder, a casual gamer, and just the
+right kind of neurotic that I can function 
+as if naturally disciplined and organized.
+	</p>
+<p>
+I worked in social work for several years after
+college before discovering my career in public
+libraries. After some years in a public facing
+role working with all ages in various roles,
+I began work in digital services, and soon 
+moved into management and administration.
+
+	</p>
+<p>
+WORK &amp; CAREER
+I originally studied religion and psychology,
+and spent several years doing inpatient and
+outpatient social work. But since 2003, I have
+been working in Public Libraries, first in a
+public facing role, planning and running 
+programs for children, teens, and adults. 
+	</p>
+<p>
+In 2010, I was recruited for my library system's
+Digital Library, providing support to staff and
+patrons related to our digital services, as well
+as creating custom web interfaces for internal
+and public use. 
+	</p>
+<p>
+In 2014, I began managing the Digital Library
+before moving into a interim role as Senior
+Library Manager for Digital Infrastructure, 
+overseeing Library IT, Marketing &amp; Awareness,
+and Library Information Services (responsible 
+for the library system's web presence and the
+Integrated Library System software). 
+	
+</p>
+	
+<p>
+	
+HOME
+I am a dad, an artist, a writer, a librarian, 
+a web coder, a casual gamer, and just the
+right kind of neurotic that I can function 
+as if naturally disciplined and organized.
+	</p>
+<p>
+I worked in social work for several years after
+college before discovering my career in public
+libraries. After some years in a public facing
+role working with all ages in various roles,
+I began work in digital services, and soon 
+moved into management and administration.
+
+	</p>
+<p>
+WORK &amp; CAREER
+I originally studied religion and psychology,
+and spent several years doing inpatient and
+outpatient social work. But since 2003, I have
+been working in Public Libraries, first in a
+public facing role, planning and running 
+programs for children, teens, and adults. 
+	</p>
+<p>
+In 2010, I was recruited for my library system's
+Digital Library, providing support to staff and
+patrons related to our digital services, as well
+as creating custom web interfaces for internal
+and public use. 
+	</p>
+<p>
+In 2014, I began managing the Digital Library
+before moving into a interim role as Senior
+Library Manager for Digital Infrastructure, 
+overseeing Library IT, Marketing &amp; Awareness,
+and Library Information Services (responsible 
+for the library system's web presence and the
+Integrated Library System software). 
+	
+</p
+	<p>
+	
+HOME
+I am a dad, an artist, a writer, a librarian, 
+a web coder, a casual gamer, and just the
+right kind of neurotic that I can function 
+as if naturally disciplined and organized.
+	</p>
+<p>
+I worked in social work for several years after
+college before discovering my career in public
+libraries. After some years in a public facing
+role working with all ages in various roles,
+I began work in digital services, and soon 
+moved into management and administration.
+
+	</p>
+<p>
+WORK &amp; CAREER
+I originally studied religion and psychology,
+and spent several years doing inpatient and
+outpatient social work. But since 2003, I have
+been working in Public Libraries, first in a
+public facing role, planning and running 
+programs for children, teens, and adults. 
+	</p>
+<p>
+In 2010, I was recruited for my library system's
+Digital Library, providing support to staff and
+patrons related to our digital services, as well
+as creating custom web interfaces for internal
+and public use. 
+	</p>
+<p>
+In 2014, I began managing the Digital Library
+before moving into a interim role as Senior
+Library Manager for Digital Infrastructure, 
+overseeing Library IT, Marketing &amp; Awareness,
+and Library Information Services (responsible 
+for the library system's web presence and the
+Integrated Library System software). 
+	
+</p>
+	
+<div class="w3-padding-small w3-text-theme scroll-top">TOP!</div>
 </body>
 	
 <script type="application/javascript">
 
 $(document).ready(function(){
-  // Add smooth scrolling to all links
-  $(".scrollTop").on('click', function(event) {
-	  alert("hello?");
-
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
-
-      // Store hash
-      var hash = this.hash;
-
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
-  });
+    $(".scroll-top").click(function() {
+        $("html, body").animate({ 
+            scrollTop: 0 
+        }, "slow");
+        return false;
+    });
 });
 </script>
 	
